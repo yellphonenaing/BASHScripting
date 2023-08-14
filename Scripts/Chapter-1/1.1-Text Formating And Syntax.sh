14a795982680d35d39bb73ccd66fcc131bde96f0
ypn007 <55645320+ypn007@users.noreply.github.com>
2023-08-14 03:06:45 -0400

    Update 1.1-Text Formating And Syntax.sh

--- Scripts/Chapter-1/1.1-Text Formating And Syntax.sh
+++ Scripts/Chapter-1/1.1-Text Formating And Syntax.sh
@@ -1,1 +1,15 @@
 #!/usr/bin/bash
+#This is command
+
+ls; pwd # usage of ;
+id && whoami # usage of &&
+zsh || bash # usage of double pipe
+cat /etc/passwd | head -n 10
+echo "Hello world"
+echo -e "\e[1;32mHello World\e[0m"
+echo -e "\e[1;41mHello World\e[0m"
+echo -e "\e[1;32;41mHello World\e[0m"
+echo -e "\e[5;32;41mHello World\e[0m"
+echo -n "Working => "
+sleep 2
+echo "Success"
